fc7379d6f22919de96ab84a33a6dd3e5dd96e6da
JIanhuaLi-CASC <lijh14@163.com>
2022-03-27 22:19:58 +0800

    chessboard

--- ConsoleApplication1/tinyraytracer.cpp
+++ ConsoleApplication1/tinyraytracer.cpp
@@ -18,8 +18,8 @@ public:
 	float& operator[](const int i) { return i == 0 ? x : (1 == i ? y : z); }
 	const float& operator[](const int i) const { return i == 0 ? x : (1 == i ? y : z); }
 	vec3  operator*(const float v) const { return{ x*v, y*v, z*v }; }
-	float operator*(const vec3& v) const { return x*v.x + y*v.y + z*v.z; }
-	vec3  operator/(const float& v) const { return{ x/v, y/v, z/v }; }
+	float operator*(const vec3& v) const { return x*v.x + y*v.y + z*v.z; }//inner product
+	vec3  operator/(const float& v) const { return{ x / v, y / v, z / v }; }
 	vec3  operator+(const vec3& v) const { return{ x + v.x, y + v.y, z + v.z }; }
 	vec3  operator-(const vec3& v) const { return{ x - v.x, y - v.y, z - v.z }; }
 	vec3  operator-()              const { return{ -x, -y, -z }; }
@@ -35,8 +35,8 @@ public:
 class Material {
 public:
 	Material(const vec3 &color, const float &exp, const vec3 &a, const float &indx, const float &fct) :
-		color(color), specular_exponent(exp), albedo(a), refra_index(indx), refra_factor(fct){}
-	Material() : color(0.f, 0.f, 0.f), specular_exponent(0.f), albedo(0.f,0.f,0.f), refra_index(1.f), refra_factor(0.f) {}
+		color(color), specular_exponent(exp), albedo(a), refra_index(indx), refra_factor(fct) {}
+	Material() : color(0.f, 0.f, 0.f), specular_exponent(0.f), albedo(0.f, 0.f, 0.f), refra_index(1.f), refra_factor(0.f) {}
 	vec3 color;
 	float specular_exponent;//镜面反射的指数因子，越小角度越集中
 	vec3 albedo;//反照的比例，如果为1则只有镜面反射，没有漫反射
@@ -56,10 +56,10 @@ vec3 reflect(const vec3 &I, const vec3 &N) {
 
 vec3 refract(const vec3 &I, const vec3 &N, const float &ref_index) {
 	vec3 tmp;
-	if (I*N<0)//from outside to inside
-		tmp=(I - N*(I*N)) / ref_index + N * ( I * N );
+	if (I*N < 0)//from outside to inside
+		tmp = (I - N*(I*N)) / ref_index + N * (I * N);
 	else
-		tmp=(I - N*(I*N)) * ref_index + N * (I * N);
+		tmp = (I - N*(I*N)) * ref_index + N * (I * N);
 	return tmp.normalized();
 }
 
@@ -99,19 +99,66 @@ public:
 	Material mat;
 };
 
+
+class chessboard
+{
+public:
+	//sphere();
+	//~sphere();
+	chessboard(const vec3 &n, const vec3 &c, const float &l, const int &num, const vec3 c1, const vec3 c2)
+		: normal(n), center(c), length(l), num(num), color1(c1), color2(c2) {}
+	//	chessboard() { normal = { 0 }; };
+	//计算直线与chessboard的交点。dir 必须为归一化矢量，一个从orig点发射的，方向为dir的直线，
+	//与chessbord相交点距离直线的距离为t0
+	//计算方法：直线方程为x = x0 + dir_x * t;y = y0 + dir_y * t,z = z0 + dir_z * t
+	//把直线方程代入平面方程，得到t：n1*(x-c1)+n2*(y-c2)+n3*(z-c3)=0
+	//n1 * (x0+dir_x*t-c1)+n2 * (y0+dir_y*t-c2)+n3 * (z0+dir_z*t-c3)=0
+	//t = -(n *( orig-c))/(n*dir)
+	std::tuple<bool, float, vec3, vec3>  ray_intersect(const vec3 &orig, const vec3 &dir) const
+	{
+		if (fabs(normal*dir) < 1e-6) return  std::make_tuple(false, 0, vec3(0, 0, 0), vec3(0, 0, 0));
+		float t = -(normal * (orig - center)) / (normal*dir);
+		vec3 hit = orig + dir*t;
+		vec3 c2p = hit - center;
+		//判断交点是否在length范围内，判断颜色
+		int x_num = fabsf(c2p[0]) / length;
+		int z_num = fabsf(c2p[2]) / length;
+
+		if (t > 0 && x_num < num  && z_num < num)
+		{	
+			if ((x_num + z_num) % 2 == 0)
+				return  std::make_tuple(true, t, hit, color1);
+			else
+				return  std::make_tuple(true, t, hit, color2);
+		}
+		else return  std::make_tuple(false, 0, vec3(0, 0, 0), vec3(0, 0, 0));
+	}
+	vec3 normal;//法线方向
+	vec3 center;//中心点
+	float length = 0.5f;//单个chessboard的宽度
+	int num = 2;//how many chess board along one direction
+	vec3 color1 = vec3(1, 1, 0);//yellow
+	vec3 color2 = vec3(1, 1, 1);//two color
+};
+
+
+
+
+
 //渲染函数
 void render() {
 
 }
 
-bool scene_intersect(const vec3 &orig, const vec3 &dir, const std::vector<sphere> &spheres, vec3 &hit, vec3 &N, Material &mat) {
+bool scene_intersect(const vec3 &orig, const vec3 &dir, const std::vector<sphere> &spheres, 
+	chessboard &chessbd,vec3 &hit, vec3 &N, Material &mat) {
 	float dist_sphere = std::numeric_limits<float>::max();
 	float dist_i = std::numeric_limits<float>::max();
 	vec3 top_c;
 	//	vec3  hit;
 	for (size_t i = 0; i < spheres.size(); i++)
 	{
-		if (spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < dist_sphere)//相交
+		if (spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < dist_sphere)//相交,多个球，取最近的
 		{
 			dist_sphere = dist_i;
 			hit = orig + dir*dist_i;
@@ -120,25 +167,45 @@ bool scene_intersect(const vec3 &orig, const vec3 &dir, const std::vector<sphere
 			mat = spheres[i].mat;
 		}
 	}
+	
+	bool hit_flag;
+	float dist_chessbd;
+	vec3 hit_point, hit_color;
+	std::tie(hit_flag, dist_chessbd, hit_point, hit_color) = chessbd.ray_intersect(orig, dir);
+
+	if (dist_sphere < dist_chessbd)
+	
 	if (dist_sphere > 1000)//都不相交
 		return false;
 	else
 		return true;
 }
 
-vec3 cast_ray(const vec3 &orig, const vec3 &dir, const std::vector<sphere> &spheres, const std::vector<Light> &lights, size_t depth = 0)
+//补充chessboard的相交判断
+//bool scene_intersect(const vec3 &orig, const vec3 &dir, vec3 &hit, chessboard &chessbd) {
+//	//	float dist_sphere = std::numeric_limits<float>::max();
+//	float dist_i = std::numeric_limits<float>::max();
+//	vec3 top_c;	//	vec3  hit;
+//
+//}
+
+
+
+
+vec3 cast_ray(const vec3 &orig, const vec3 &dir, const std::vector<sphere> &spheres, 
+			const std::vector<Light> &lights, const chessboard &chessbd, size_t depth = 0)
 {
 	vec3 N, hit, light_dir;
 	float light_distance;
 	vec3 color;
 	Material mat;
 
-	if (depth > 2 || !scene_intersect(orig, dir, spheres, hit, N, mat)) //depth表示光线追迹的次数
+	if (depth > 4 || !scene_intersect(orig, dir, spheres, hit, N, mat)) //depth表示光线追迹的次数
 		return color = vec3(0.2, 0.2, 0.2);//backgroud color
 
 	vec3 reflect_dir = reflect(dir, N).normalized();
 	//vec3 reflect_orig = reflect_dir*N < 0 ? hit - N*1e-3 : hit + N*1e-3; // offset the original point to avoid occlusion by the object itself
-	vec3 reflect_orig =  hit + reflect_dir*1e-3;
+	vec3 reflect_orig = hit + reflect_dir*1e-3;
 	vec3 reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1);//函数嵌套
 
 	vec3 refract_dir = refract(dir, N, mat.refra_index);
@@ -162,8 +229,8 @@ vec3 cast_ray(const vec3 &orig, const vec3 &dir, const std::vector<sphere> &sphe
 		specular_light_intensity += powf(std::max(0.f, reflect(light_dir, N)*dir), mat.specular_exponent)*lights[k].intensity;
 	}
 	color = mat.color * diffuse_light_intensity * mat.albedo[0]
-		+ vec3(0.2, 0.2, 0.2) * specular_light_intensity * mat.albedo[1] 
-		+ reflect_color*mat.albedo[2] + refract_color * ( mat.refra_factor);
+		+ vec3(0.2, 0.2, 0.2) * specular_light_intensity * mat.albedo[1]
+		+ reflect_color*mat.albedo[2] + refract_color * (mat.refra_factor);
 
 	if (color.x > 1)	color.x = 1;
 	if (color.y > 1)	color.y = 1;
@@ -183,11 +250,11 @@ void main()
 	float fov = 60;//相机视角大小，相机为原点，屏幕图像为0 0 1平面，也就是说焦距为1
 	float d = 2 * tan(fov / 2 / 180.0 * PI) / (float)width;//每个像素的尺寸
 
-	Material   red(vec3(0.2, 0.6, 0.1), 20, vec3(0.5,0.2,0.1),1.5,0.0);
-	Material green(vec3(0.1, 0.1, 0.1), 500, vec3(0.1, 0.5, 0.5),1.5,1);//折射增强
-	Material blue(vec3(0.6, 0.3, 0.2), 20, vec3(0.5, 0.2, 0.1),1.5,0.0);
-//	Material glass(vec3(0.6, 0.7, 0.8), 200, vec3(0.1, 0.1, 0.1),1.5,0.1);
-	Material mirror(vec3(0.1, 0.2, 0.3), 1500, vec3(0.1, 1, 1),1.5,0.3);//反射增强
+	Material   red(vec3(0.2, 0.6, 0.1), 20, vec3(0.5, 0.2, 0.1), 1.5, 0.0);
+	Material green(vec3(0.1, 0.1, 0.1), 500, vec3(0.1, 0.5, 0.5), 1.5, 1);//折射增强
+	Material blue(vec3(0.6, 0.3, 0.2), 20, vec3(0.5, 0.2, 0.1), 1.5, 0.0);
+	//	Material glass(vec3(0.6, 0.7, 0.8), 200, vec3(0.1, 0.1, 0.1),1.5,0.1);
+	Material mirror(vec3(0.1, 0.2, 0.3), 1500, vec3(0.1, 1, 1), 1.5, 0.3);//反射增强
 
 	std::vector<sphere> spheres;
 
@@ -208,6 +275,14 @@ void main()
 	lights.push_back(light1);
 	lights.push_back(light2);
 	lights.push_back(light3);
+
+
+	vec3 norm(0, 1, 0), center(0, -1, 9);
+	vec3 yellow(1, 1, 0), white(1, 1, 1);
+	chessboard chessbd(norm, center, 0.6, 5, yellow, white);
+
+
+
 	//	char* filename = "step3.jpg";//more spheres
 	//	char* filename = "step4.jpg";//light,需要知道交点位置
 	unsigned char* framebuffer;
@@ -218,18 +293,26 @@ void main()
 	vec3 clr;
 	Material mat;
 	float diffuse_light_intensity = 0, specular_light_intensity = 0;
-	for (int i = 0; i < height; i++)
-		for (int j = 0; j < width; j++)
+	for (int i = 0; i < height; i++)//y坐标
+		for (int j = 0; j < width; j++)//x坐标
 		{
 			dir = vec3((j - width / 2) * d, (-i + height / 2) * d, 1);//坐标系，向右为x，向上为y，正前为z
 			dir = dir.normalized();//从相机过来的光线矢量
 
-			clr = cast_ray(orig, dir, spheres, lights);
+			//clr = cast_ray(orig, dir, spheres, lights);
+
+			bool hit_flag;
+			float hit_dis;
+			vec3 hit_point, hit_color;
+			std::tie(hit_flag, hit_dis, hit_point, hit_color) = chessbd.ray_intersect(orig, dir);
+
+			if (hit_flag)  clr = hit_color;
+			else clr = vec3(0.1, 0.2, 0.3);
 
 			framebuffer[index++] = (unsigned char)(clr.x * 255);
 			framebuffer[index++] = (unsigned char)(clr.y * 255);
 			framebuffer[index++] = (unsigned char)(clr.z * 255);
 		}
 
-	stbi_write_bmp("step8-refraction.bmp", width, height, comp, framebuffer);
+	stbi_write_bmp("step9_chessborad.bmp", width, height, comp, framebuffer);
 }
